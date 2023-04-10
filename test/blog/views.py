a2665c80dfe2c3793c06594b5878ebf445c50737
apfl99 <rmrehd604@gmail.com>
2023-04-10 18:32:58 +0900

    CBV page

--- test/blog/views.py
+++ test/blog/views.py
@@ -4,6 +4,10 @@ from .models import Post
 
 # Create your views here.
 
+# 08 웹 페이지 만들기 - CBV로 페이지 만들기 참조
+# class PostList(ListView): # 모델명_list.html
+#     model = Post
+#     template_name = 'blog/index.html' # 템플릿 지정
 
 
 def index(request):
