bcf62b36a561ca2306a5c2260f3e69ff089e93c3
apfl99 <rmrehd604@gmail.com>
2023-04-10 18:28:43 +0900

    FBV page

---
+++ test/single_pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.landing),
+    path('about_me/',views.about_me),
+
+
+
+    path("blog/", include('blog.urls')),
+]
